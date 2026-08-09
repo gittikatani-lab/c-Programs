455c7eba3bf4a21a4709d298ae167276c705d8ee
gittikatani-lab <gittikatani@gmail.com>
2026-08-09 21:51:43 +0530

    Add files via upload

---
+++ tryy.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int a,b;
+    printf("enter number a");
+    scanf("%d", &a);
+    printf("enter number b");
+    scanf("&d", &b);
+    int sum=a+b;
+    printf("sum of no.=%d", sum);
+    return 0;
+}
