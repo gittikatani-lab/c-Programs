fa6591ae66fa06c97b01c7a6b25bea6a91069f62
gittikatani-lab <gittikatani@gmail.com>
2026-08-10 17:19:14 +0530

    Add files via upload

---
+++ a.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+//area of square
+int main() {
+    float side;
+    printf("enter side");
+    scanf("%f", &side);
+
+    printf("area of square is: %f", side*side);
+    return 0;
+    
+}
