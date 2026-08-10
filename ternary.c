38b9075348f2dbba08a2d8b7a9d368acd310f5b1
gittikatani-lab <gittikatani@gmail.com>
2026-08-10 09:03:55 +0530

    Add files via upload

---
+++ ternary.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+    int age;
+    printf("enter age");
+    scanf("%d", &age);
+    printf("age is =%d", age);
+age>=18 ? printf("adult\n"):printf("not adult\n");
+
+
+
+
+
+
+
+
+}
