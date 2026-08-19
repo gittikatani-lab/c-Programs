b73a8345b644b5bc654418f7a3125780587a32be
Gitika <gittikatani@gmail.com>
2026-08-19 19:19:09 +0530

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main() {
+    int day ; //1-monday;2-tuesday;3-wednesday;4-thursday;5-friday;6-saturday;7-sunday
+    printf("enter day(1-7):");
+    scanf("%d",&day);
+
+    switch (day) {
+        case 1: printf("monday\n");
+        break;
+        case 2: printf("tuesday\n");
+        break;
+        case 3: printf("wednesday\n");
+        break;
+        case 4: printf("thursday\n");
+        break;
+        case 5: printf("friday\n");
+        break;
+        case 6: printf("saturday\n");
+        break;
+        case 7: printf("sunday\n");
+        break;
+        default :
+        printf("not in list");
+        break;
+    }
+    
+    return 0;
+}
